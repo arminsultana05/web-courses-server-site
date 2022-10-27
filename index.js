74450edac66eca8349987be0e435df4dd8e52350
arminsultana <arminsultana556@gmail.com>
2022-10-27 15:27:23 +0600

    user data commited

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ async function run() {
     try {
         await client.connect();
         const courseCollection = client.db('web-courses').collection('courses')
+        const userCollections = client.db('web-courses').collection('users');
 
         //Get all data method
         app.get('/course', async (req, res) => {
@@ -34,7 +35,28 @@ async function run() {
             const query ={_id: ObjectId(id)};
             const course =await courseCollection.findOne(query);
             res.send(course)
-        })  
+        }) 
+        
+        //Save all user in database by email
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+      
+            const filter = { email: email };
+            const option = { upsert: true };
+            const updateDoc = {
+              $set: user,
+            };
+            const result = await userCollections.updateOne(filter, updateDoc, option)
+            res.send(result)
+          });
+
+          //get all user..
+          app.get('/users', async (req, res) => {
+            const user = await userCollections.find().toArray();
+            res.send(user)
+          })
+      
 
     }
     finally {
